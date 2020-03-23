7324037fc964668adc98ed0c2149c5b6666cc23a
Ayoola FALOLA <ayoola@ayoo.la>
2020-03-23 10:50:12 +0100

    Create widgets to use

---
+++ application/modules/Workplace/Keylog/Table.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * PageCarton
+ *
+ * LICENSE
+ *
+ * @category   PageCarton
+ * @package    Workplace_Keylog_Table
+ * @copyright  Copyright (c) 2020 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: Table.php Monday 23rd of March 2020 09:45AM ayoola@ayoo.la $
+ */
+
+/**
+ * @see PageCarton_Table
+ */
+
+
+class Workplace_Keylog_Table extends PageCarton_Table
+{
+
+    /**
+     * The table version (SVN COMPATIBLE)
+     *
+     * @param string
+     */
+    protected $_tableVersion = '0.0';  
+
+    /**
+     * Table data types and declaration
+     * array( 'fieldname' => 'DATATYPE' )
+     *
+     * @param array
+     */
+	protected $_dataTypes = array (
+  'texts' => 'INPUTTEXT',
+  'user_id' => 'INPUTTEXT',
+  'window_id' => 'INPUTTEXT',
+);
+
+
+	// END OF CLASS
+}
---
+++ application/modules/Workplace/Keylog/Table/Abstract.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * PageCarton
+ *
+ * LICENSE
+ *
+ * @category   PageCarton
+ * @package    Workplace_Keylog_Table_Abstract
+ * @copyright  Copyright (c) 2020 PageCarton (http://www.pagecarton.org)
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @version    $Id: Abstract.php Monday 23rd of March 2020 09:45AM ayoola@ayoo.la $
+ */
+
+/**
+ * @see PageCarton_Widget
+ */
+
+
+class Workplace_Keylog_Table_Abstract extends PageCarton_Widget
+{
+	
+    /**
+     * Identifier for the column to edit
+     * 
+     * @var array
+     */
+	protected $_identifierKeys = array( 'table_id' );
+ 	
+    /**
+     * The column name of the primary key
+     *
+     * @var string
+     */
+	protected $_idColumn = 'table_id';
+	
+    /**
+     * Identifier for the column to edit
+     * 
+     * @var string
+     */
+	protected $_tableClass = 'Workplace_Keylog_Table';
+	
+    /**
+     * Access level for player. Defaults to everyone
+     *
+     * @var boolean
+     */
+	protected static $_accessLevel = array( 99, 98 );
+
+
+    /**
+     * creates the form for creating and editing page
+     * 
+     * param string The Value of the Submit Button
+     * param string Value of the Legend
+     * param array Default Values
+     */
+	public function createForm( $submitValue = null, $legend = null, Array $values = null )  
+    {
+		//	Form to create a new page
+        $form = new Ayoola_Form( array( 'name' => $this->getObjectName(), 'data-not-playable' => true ) );
+		$form->submitValue = $submitValue ;
+//		$form->oneFieldSetAtATime = true;
+
+		$fieldset = new Ayoola_Form_Element;
+	//	$fieldset->placeholderInPlaceOfLabel = false;       
+        $fieldset->addElement( array( 'name' => 'texts', 'type' => 'InputText', 'value' => @$values['texts'] ) );         $fieldset->addElement( array( 'name' => 'user_id', 'type' => 'InputText', 'value' => @$values['user_id'] ) );         $fieldset->addElement( array( 'name' => 'window_id', 'type' => 'InputText', 'value' => @$values['window_id'] ) ); 
+
+		$fieldset->addLegend( $legend );
+		$form->addFieldset( $fieldset );   
+		$this->setForm( $form );
+    } 
+
+	// END OF CLASS
+}
