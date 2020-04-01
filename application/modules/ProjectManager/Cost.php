338912834779126bd9b45524bb395c6fd07ca147
Ayoola FALOLA <ayoola@ayoo.la>
2020-04-01 13:14:12 +0100

    Removed project manager plugin

--- application/modules/ProjectManager/Cost.php
+++
@@ -1,1 +0,0 @@
-/Users/ayoolafalola/pagecarton/pagecarton/sites/default/workplace/extensions/project_manager/application/modules/ProjectManager/Cost.php
--- application/pages/includes/post-viewer-managed-project.php
+++
@@ -1,1 +0,0 @@
-/Users/ayoolafalola/pagecarton/pagecarton/sites/default/workplace/extensions/project_manager/application/pages/includes/post-viewer-managed-project.php
